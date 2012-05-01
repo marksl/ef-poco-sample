61dee258f52ed79a2d1d062b90a8728f4d8b963a
Mark Lamley <mark.lamley@gmail.com>
2012-05-01 07:17:37 -0700

    add all

---
+++ OData/WcfDataService1.svc.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity.Infrastructure;
+using System.Data.Objects;
+using System.Data.Services;
+using System.Data.Services.Common;
+using Model;
+
+namespace OData
+{
+    public class WcfDataService1 : DataService<ObjectContext>
+    {
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            config.SetEntitySetAccessRule("Contacts", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("ContactGroups", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("Users", EntitySetRights.AllRead);
+
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+            config.UseVerboseErrors = true;
+        }
+
+        protected override ObjectContext CreateDataSource()
+        {
+            var ctx = new servicestackEntities();
+            var objectContext = ((IObjectContextAdapter)ctx).ObjectContext;
+            objectContext.ContextOptions.ProxyCreationEnabled = false;
+            return objectContext;
+        }
+
+    }
+}
